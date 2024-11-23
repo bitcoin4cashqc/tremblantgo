977020633d02673ea4cb622f1140fddf754fdba7
bitcoin4cashqc <bitcoin4cashqc@hotmail.com>
2024-11-23 00:26:21 -0500

    not sure if all that needed but

--- Capacitor/android/app/src/main/java/com/tremblantgo/app/MainActivity.java
+++ Capacitor/android/app/src/main/java/com/tremblantgo/app/MainActivity.java
@@ -3,41 +3,18 @@ package com.tremblantgo.app;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
-import android.util.Log;
 
 public class MainActivity extends BridgeActivity {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Initialize Firebase
         FirebaseApp.initializeApp(this);
-
-        FirebaseMessaging.getInstance().getToken()
-        .addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-                Log.e("FCM", "Fetching FCM token failed", task.getException());
-                return;
-            }
-            String token = task.getResult();
-            Log.d("FCM", "FCM Token: " + token);
-        });
-
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1001);
-            }
-        }
-    }
+  
 }
